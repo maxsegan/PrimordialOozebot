bb9c8b296c860ab5c13e985604a76b82f3dbccf8
Msegan <mms2305@columbia.edu>
2020-12-08 13:02:02 -0500

    Learning curve

--- plot.py
+++ plot.py
@@ -3,40 +3,21 @@ import csv
 import statistics
 import math
 
-plt.title('Population Diversity')
-plt.ylabel('Diversity Score')
+plt.title('Learning Curve')
+plt.ylabel('Speed')
 plt.xlabel('Iteration Number')
 
-random = []
-randombars = []
-rmin = []
-rmax = []
-hill = []
-hillbars = []
-hmin = []
-hmax = []
 evo = []
 emin = []
 emax = []
 evobars = []
-cross = []
-crossbars = []
-cmin = []
-cmax = []
-numRuns = 5
-numIterations = 100000000
-sqrtRuns = math.sqrt(numRuns)
-iterationDataRandom = []
-iterationDataHill = []
-iterationDataEvo = []
-iterationDataCross = []
 
-indicesToPlot = [10, 15, 20, 25]
-index = 60
-while indicesToPlot[-1] < numIterations:
-    indicesToPlot.append(index)
-    index = int(index * 1.02)
-indicesToPlot[-1] = numIterations - 1
+numRuns = 5
+numIterations = 20700
+sqrtRuns = math.sqrt(numRuns)
+iterationDataEvo = []
+
+indicesToPlot = range(1, numIterations)
 
 #xtiks = []
 #for i in range(10):
@@ -44,88 +25,24 @@ indicesToPlot[-1] = numIterations - 1
 #plt.xticks(xtiks)
 
 for i in range(1, numRuns + 1):
-    iterationDataRandom.append({})
-    iterationDataHill.append({})
     iterationDataEvo.append({})
-    iterationDataCross.append({})
-    with open('rand' + str(i) + '.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        index = 0
-        for row in reversed(list(reader)):
-            vals = row[0].split(',')
-            iteration = int(vals[0])
-            val = float(vals[1])
-
-            while index < len(indicesToPlot) - 1 and indicesToPlot[index + 1] < iteration:
-                index += 1
-            
-            iterationDataRandom[-1][indicesToPlot[index]] = val
-    with open('hill' + str(i) + '.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        index = 0
-        for row in reversed(list(reader)):
-            vals = row[0].split(',')
-            iteration = int(vals[0])
-            val = float(vals[2])
-
-            while index < len(indicesToPlot) - 1 and indicesToPlot[index] < iteration:
-                index += 1
-            iterationDataHill[-1][indicesToPlot[index]] = val
     with open('evo' + str(i) + '.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        index = 0
-        for row in reversed(list(reader)):
-            vals = row[0].split(',')
-            iteration = int(vals[0]) * 100
-            val = float(vals[2])
-            while index < len(indicesToPlot) - 1 and indicesToPlot[index] < iteration:
-                index += 1
-            
-            iterationDataEvo[-1][indicesToPlot[index]] = val
-    with open('ed' + str(i) + '.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        index = 0
-        for row in reversed(list(reader)):
+        for row in list(reader):
             vals = row[0].split(',')
             iteration = int(vals[0])
-            val = float(vals[2])
-            while index < len(indicesToPlot) - 1 and indicesToPlot[index] < iteration:
-                index += 1
+            val = float(row[1]) / 5.0
+            iterationDataEvo[-1][iteration] = val
 
-            iterationDataCross[-1][indicesToPlot[index]] = val
 print("Done reading data")
 
-unifiedRandom = []
-unifiedHill = []
 unifiedEvo = []
-unifiedCross = []
+
 index = 0
 for iteration in indicesToPlot:
-    currentRandom = []
-    currentHill = []
     currentEvo = []
-    currentCross = []
-    unifiedRandom.append(currentRandom)
-    unifiedHill.append(currentHill)
     unifiedEvo.append(currentEvo)
-    unifiedCross.append(currentCross)
     for run in range(numRuns):
-        valRandom = -1
-        if iteration in iterationDataRandom[run]:
-            valRandom = iterationDataRandom[run][iteration]
-        else:
-            # unchanged
-            valRandom = unifiedRandom[-2][run]
-        currentRandom.append(valRandom)
-
-        valHill = -1
-        if iteration in iterationDataHill[run]:
-            valHill = iterationDataHill[run][iteration]
-        else:
-            # unchanged
-            valHill = unifiedHill[-2][run]
-        currentHill.append(valHill)
-
         valEvo = -1
         if iteration in iterationDataEvo[run]:
             valEvo = iterationDataEvo[run][iteration]
@@ -134,64 +51,22 @@ for iteration in indicesToPlot:
             valEvo = unifiedEvo[-2][run]
         currentEvo.append(valEvo)
 
-        valCross = -1
-        if iteration in iterationDataCross[run]:
-            valCross = iterationDataCross[run][iteration]
-        else:
-            #unchanged
-            valCross = unifiedCross[-2][run]
-        currentCross.append(valCross)
-
-    randomAverage = statistics.mean(currentRandom)
-    randomError = statistics.stdev(currentRandom) / sqrtRuns
-
-    random.append(randomAverage)
-    randombars.append(randomError)
-
-    hillAverage = statistics.mean(currentHill)
-    hillError = statistics.stdev(currentHill) / sqrtRuns
-
-    hill.append(hillAverage)
-    hillbars.append(hillError)
-
     evoAverage = statistics.mean(currentEvo)
     evoError = statistics.stdev(currentEvo) / sqrtRuns
 
     evo.append(evoAverage)
     evobars.append(evoError)
 
-    crossAverage = statistics.mean(currentCross)
-    crossError = statistics.stdev(currentCross) / sqrtRuns
-
-    cross.append(crossAverage)
-    crossbars.append(crossError)
-
-for i in range(len(random)):
-    rmin.append(random[i] - randombars[i])
-    rmax.append(random[i] + randombars[i])
-
-    hmin.append(hill[i] - hillbars[i])
-    hmax.append(hill[i] + hillbars[i])
-
+for i in range(len(evo)):
     emin.append(evo[i] - evobars[i])
     emax.append(evo[i] + evobars[i])
 
-    cmin.append(cross[i] - crossbars[i])
-    cmax.append(cross[i] + crossbars[i])
-
 print("Done processing data")
 
-plt.xscale('log')
+#plt.xscale('log')
 #plt.yscale('log')
-#plt.plot(indicesToPlot, random, color='blue', linewidth=1, label='Random Search')
-plt.plot(indicesToPlot, hill, color='green', linewidth=1, label='Parallel Hill Climb')
-plt.plot(indicesToPlot, evo, color='red', linewidth=1, label='Weighted Selection')
-plt.plot(indicesToPlot, cross, color='blue', linewidth=1, label='Parental Replacement')
-plt.fill_between(indicesToPlot, hmin, hmax, facecolor='green', lw=0, alpha=0.5)
-plt.fill_between(indicesToPlot, emin, emax, facecolor='red', lw=0, alpha=0.5)
-plt.fill_between(indicesToPlot, cmin, cmax, facecolor='blue', lw=0, alpha=0.5)
-#plt.fill_between(indicesToPlot, rmin, rmax, facecolor='blue', lw=0, alpha=0.5)
+plt.plot(indicesToPlot, evo, color='blue', linewidth=1)
+plt.fill_between(indicesToPlot, emin, emax, facecolor='blue', lw=0, alpha=0.5)
 
-plt.legend(loc='best')
-plt.savefig('diversityp.png', dpi=500)
+plt.savefig('learningcurve.png', dpi=500)
 plt.show()
